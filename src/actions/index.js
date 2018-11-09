cbb4dfc6ba113bdff5a8e78879f1f56f51c4a868
kanmir <kiselev.dmitrii@mail.ru>
2018-11-09 16:41:35 +0300

    Added weather reducer and container

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,15 @@
-export function selectBook(book) {
+const API_KEY = 'f4bd4e6beaf776868efca74bf3421e50';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city}`;
+    const request = fetch(url)
+        .then(response => response.json())
+        .catch((e) => {return {type: FETCH_WEATHER, payload: e} });
     return {
-        type: 'BOOK_SELECTED',
-        payload: book
+        type: FETCH_WEATHER,
+        payload: request
     }
 }
